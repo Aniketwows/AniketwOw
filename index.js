9c92f0e38982266542709779743ed90ee4b2ba77
AniketwOw <aniketwows09@gmail.com>
2026-01-16 02:42:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new Client({
 
 /* ================= CONFIG ================= */
 const ROLE_NAME = "Aniketshare/Noti";
-const BRAND_COLOR = 0xbcbcbc;
+const BRAND_COLOR =  0x34343c;
 
 /* ================= AUTO STATUS ================= */
 const statuses = [
